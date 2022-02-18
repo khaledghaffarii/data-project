187ad9323c3ced28021225edd6831f024d768d99
khaledghaffarii <med.khaledghaffary@gmail.com>
2022-02-18 11:58:15 +0100

    fix header.js

--- components/Header.js
+++ components/Header.js
@@ -84,7 +84,7 @@ function Header() {
                 >
                   <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
-                      <form method="POST" action="#">
+                      <form>
                         <Menu.Item>
                           {({ active }) => (
                             <button
