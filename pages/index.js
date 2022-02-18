e77bb936423d567cf37e43f9cf3905e65e4786c3
khaledghaffarii <med.khaledghaffary@gmail.com>
2022-02-18 10:25:09 +0100

    fix index.js

--- pages/index.js
+++ pages/index.js
@@ -15,19 +15,19 @@ export default function Home({}) {
   )
 }
 
-export async function getStaticProps() {
-  const exploreData = await fetch('https://links.papareact.com/pyp').then(
-    (res) => res.json()
-  )
+// export async function getStaticProps() {
+//   const exploreData = await fetch('https://links.papareact.com/pyp').then(
+//     (res) => res.json()
+//   )
 
-  const cardsData = await fetch('https://links.papareact.com/zp1').then((res) =>
-    res.json()
-  )
+//   const cardsData = await fetch('https://links.papareact.com/zp1').then((res) =>
+//     res.json()
+//   )
 
-  return {
-    props: {
-      exploreData,
-      cardsData,
-    },
-  }
-}
+//   return {
+//     props: {
+//       exploreData,
+//       cardsData,
+//     },
+//   }
+// }
