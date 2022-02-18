c23d10f7479dd563152bc2ee1c8c257119f570f2
khaledghaffarii <med.khaledghaffary@gmail.com>
2022-02-18 15:43:55 +0100

    fix nextauth.js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,3 +11,11 @@ export default NextAuth({
   ],
   secret: process.env.SECRET,
 })
+
+export async function handler(req, res) {
+  // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
+  // const token = await getToken({ req })
+  const token = await getToken({ req, secret })
+  console.log('JSON Web Token', token)
+  res.end()
+}
